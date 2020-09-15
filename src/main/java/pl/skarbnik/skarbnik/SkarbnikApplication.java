7ec534a97ece435f5e2be9acc0a782d667c9058d
Dawid Dziedzic <dziedzicdev@gmail.com>
2020-09-15 12:36:38 +0200

    Initial commit

---
+++ src/main/java/pl/skarbnik/skarbnik/SkarbnikApplication.java
@@ -0,0 +1,13 @@
+package pl.skarbnik.skarbnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkarbnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkarbnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/skarbnik/skarbnik/SkarbnikApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skarbnik.skarbnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkarbnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

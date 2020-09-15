fc15990e47a107ce835abeec50f13642379018b5
Dawid Dziedzic <dziedzicdev@gmail.com>
2020-09-15 15:31:08 +0200

    Widoki w kontrolerach

--- src/main/java/pl/skarbnik/skarbnik/view/ViewController.java
+++ src/main/java/pl/skarbnik/skarbnik/view/ViewController.java
@@ -3,15 +3,34 @@ package pl.skarbnik.skarbnik.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping()
+@RequestMapping
 @Controller
 public class ViewController {
 
     @GetMapping
-    public String home(ModelMap map) {
+    public String list(ModelMap map) {
         map.put("view", "list");
+        return "general";
+    }
+
+    @GetMapping("/{id}/confirmation")
+    public String deleteConfirmation(ModelMap map, @PathVariable("id") String transactionId) {
+        map.put("view", "deleteConfirmation");
+        return "general";
+    }
+
+    @GetMapping("/create")
+    public String createTransaction(ModelMap map) {
+        map.put("view", "transactionForm");
+        return "general";
+    }
+
+    @GetMapping("/{id}/update")
+    public String editTransaction(ModelMap map, @PathVariable("id") String transactionId) {
+        map.put("view", "transactionForm");
         return "general";
     }
 

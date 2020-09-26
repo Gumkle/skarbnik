5d6c80b23a737a2fe5c2e6cce71d292176ddb690
Nights-Wolf <69758484+Nights-Wolf@users.noreply.github.com>
2020-09-26 23:32:31 +0200

    connection to database

--- src/main/java/pl/skarbnik/skarbnik/datasource/PostgresDatasource.java
+++ src/main/java/pl/skarbnik/skarbnik/datasource/PostgresDatasource.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class PostgresDatasource {
 
     @Bean
-    @ConfigurationProperties("services.database")
+    @ConfigurationProperties("app.datasource")
     public HikariDataSource hikariDataSource() {
         return DataSourceBuilder
                 .create()

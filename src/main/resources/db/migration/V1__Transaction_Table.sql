58966ba2377eb1b5f1191562f92744f7af12aef5
Nights-Wolf <69758484+Nights-Wolf@users.noreply.github.com>
2020-10-04 21:42:54 +0200

    poprawka w migracji

--- src/main/resources/db/migration/V1__Transaction_Table.sql
+++ src/main/resources/db/migration/V1__Transaction_Table.sql
@@ -1,25 +1,22 @@
-
 CREATE TABLE type (
-    Type_id INTEGER PRIMARY KEY,
-    Types VARCHAR(50)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
 );
 
 CREATE TABLE category (
-    Category_id INTEGER PRIMARY KEY,
-    Name VARCHAR(50)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
 );
 
 CREATE TABLE money (
-    Money_id INTEGER PRIMARY KEY,
-    Whole INTEGER,
-    Rest INTEGER
+    id SERIAL PRIMARY KEY,
+    whole INTEGER,
+    rest INTEGER
 );
 
 CREATE TABLE transactions (
-    Id INTEGER PRIMARY KEY,
-    Type_id INTEGER REFERENCES type (type_id),
-    Category_id INTEGER REFERENCES category (category_id),
-    Money_id INTEGER REFERENCES money (money_id)
+    id SERIAL PRIMARY KEY,
+    type INTEGER REFERENCES type (id),
+    category INTEGER REFERENCES category (id),
+    money INTEGER REFERENCES money (id)
 );
-
-
